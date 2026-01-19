57b1940ea3f53d173f330ce4e036969e0504fc0b
viczaicode <viczaimilan2003@gmail.com>
2026-01-19 15:52:10 +0100

    loading spinner,  service formatting, 2 photos

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -4,15 +4,22 @@ import { ServiceContext } from "../contexts/ServiceContext";
 function Services() {
   const { servicesLista, loading } = useContext(ServiceContext);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) return <div className="loader-center">
+  <div className="spinner"></div>
+</div>;
 
   return (
     <div className="Service-lista">
       {servicesLista.map((item) => (
         <div key={item.id} className="Service">
           <h3>{item.title}</h3>
-          <p>{item.description}</p>
+          
           <img src={item.photo} alt= {item.title} width="400" height="250"></img>
+
+          <p>{item.description}</p>
+
+          <a href="#">Order</a>
+
         </div>
       ))}
     </div>
